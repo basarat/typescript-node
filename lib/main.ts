21e67e80ad928f82716912e82c53a9300986dc83
Basarat Syed <basaratali@gmail.com>
2015-04-21 16:38:21 +1000

    WIP: initial commit

---
+++ lib/main.ts
@@ -0,0 +1,15 @@
+import a = require('example-typescript-a');
+
+var num = a.foo.a;
+var str = a.foo.b;
+console.log(num,str)
+
+import bar = require("example-typescript-a/dist/bar");
+console.log(bar.bar);
+
+import bas = require("example-typescript-a/dist/bas");
+console.log(bas.bas);
+
+import b = require("example-typescript-b");
+b.utils.log('hey');
+var shouldNotBeAny = b.momentjs;
